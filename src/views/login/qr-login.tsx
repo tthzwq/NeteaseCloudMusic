9c63927a8f2c6f9dae0a36fb52b70f9c55c31748
tthzwq <tthzwq@outlook.com>
2023-10-14 21:37:21 +0800

    [feat] 扫码登录ui

--- src/views/login/qr-login.tsx
+++ src/views/login/qr-login.tsx
@@ -1,73 +1,36 @@
-import React, { memo, useEffect, useRef, useState } from "react";
+import React, { memo, useEffect, useMemo, useRef, useState } from "react";
 import Api from "@/api/login";
-import { setCookie, openUrl } from "@/utils";
+import { setCookie, openUrl, closeCurrentWindow } from "@/utils";
 
-const QRLogin = memo(() => {
-  const [qrimg, setQrimg] = useState<string>("");
-  const [status, setStatus] = useState(0);
-  const timer = useRef<NodeJS.Timeout | undefined>();
-  const key = useRef<string>("");
+type QRStatusInfo = {
+  code?: number;
+  message?: string;
+  nickname?: string;
+  avatarUrl?: string;
+};
 
-  useEffect(() => {
-    login();
-    return reset;
-  }, []);
+type QRContentProps = {
+  qrimg: string;
+  status: number;
+  onRefresh: () => void;
+};
 
-  function reset() {
-    clearInterval(timer.current);
-    setStatus(0);
-    setQrimg("");
-  }
-
-  async function login() {
-    reset()
-    const res = await Api.getLoginQrKey();
-    key.current = res.data.unikey;
-    const res2 = await Api.loginQrCreate({ key: key.current, qrimg: true });
-    setQrimg(res2.data.qrimg);
-
-    timer.current = setInterval(async () => {
-      const statusRes = await Api.loginQrCheck({ key: key.current });
-      setStatus(statusRes.data.code);
-      console.log(statusRes.data);
-
-      if (statusRes.data.code === 800) {
-        // 二维码已过期,请重新获取
-        clearInterval(timer.current);
-      }
-      if (statusRes.data.code === 803) {
-        await Api.getLoginStatus();
-        // 授权登录成功
-        const cookie = (statusRes.rawHeaders["set-cookie"] || [])
-          .map((x: string) => x.replace(/\s*Domain=[^(;|$)]+;*/, ""))
-          .join("; ");
-        setCookie(cookie);
-        console.log(statusRes.rawHeaders["set-cookie"], cookie);
-        clearInterval(timer.current);
-      }
-    }, 1000);
-  }
-
+const QRContent = memo(({ qrimg, status, onRefresh }: QRContentProps) => {
   function handleOpen() {
     openUrl("https://music.163.com/#/download");
   }
   return (
-    <div
-      data-tauri-drag-region
-      className="w-full h-full pt-28 flex flex-col items-center"
-    >
-      <h1 className="text-3xl pb-4 font-bold">扫码登录</h1>
+    <>
       <div
         className="relative w-60 h-60 bg-[length:100%_100%]"
         style={{ backgroundImage: `url("${qrimg}")` }}
       >
-        {/* {nickname: "無肉不歡_hz", avatarUrl: "https://p2.music.126.net/My4NEKoJs4V3y1awNdIm2g==/109951163984264723.jpg", code: 802, message: "授权中"} */}
         {status === 800 && (
           <div className="flex flex-col items-center justify-center absolute w-full h-full bg-black/80 text-while">
             <div>二维码已失效</div>
             <button
               className="mt-4 px-4 py-1.5 bg-primary rounded-full"
-              onClick={login}
+              onClick={onRefresh}
             >
               点击刷新
             </button>
@@ -87,6 +50,92 @@ const QRLogin = memo(() => {
           <i className="iconfont icon-ar text-xs"></i>
         </button>
       </div>
+    </>
+  );
+});
+
+const QRLogin = memo(() => {
+  const [qrimg, setQrimg] = useState<string>("");
+  const [status, setStatus] = useState(0);
+  const qrUserInfo = useRef<QRStatusInfo>({});
+  const timer = useRef<NodeJS.Timeout | undefined>();
+  const key = useRef<string>("");
+
+  const UserContent = useMemo(() => {
+    return [802, 803].includes(status) ? (
+      <>
+        <img
+          className="mt-4 w-40 h-40 rounded-full"
+          src={qrUserInfo.current.avatarUrl}
+        />
+        <div className="mt-2 w-2/4 text-base text-center font-bol font-[500] truncate">
+          {qrUserInfo.current.nickname}
+        </div>
+        <div className="mt-8 text-sm text-[#888888] font-[500]">扫描成功</div>
+        <div className="mt-2 text-lg font-[500]">请在手机上确认登录</div>
+      </>
+    ) : null;
+  }, [status]);
+
+  useEffect(() => {
+    login();
+    return reset;
+  }, []);
+
+  function reset() {
+    key.current = "";
+    qrUserInfo.current = {};
+    clearInterval(timer.current);
+    setStatus(0);
+    setQrimg("");
+  }
+
+  async function login() {
+    reset();
+    const res = await Api.getLoginQrKey();
+    key.current = res.data.unikey;
+    const res2 = await Api.loginQrCreate({ key: key.current, qrimg: true });
+    setQrimg(res2.data.qrimg);
+
+    timer.current = setInterval(async () => {
+      const statusRes = await Api.loginQrCheck({ key: key.current });
+      setStatus(statusRes.data.code);
+      switch (statusRes.data.code) {
+        case 800: // 二维码已过期,请重新获取
+          clearInterval(timer.current);
+          break;
+        case 801: // 等待扫码
+          break;
+        case 802: // 扫描成功, 待授权
+          qrUserInfo.current = statusRes.data as QRStatusInfo;
+          break;
+        case 803: // 授权登录成功
+          // await Api.getLoginStatus();
+          const cookie = (statusRes.rawHeaders["set-cookie"] || [])
+            .map((x: string) => x.replace(/\s*Domain=[^(;|$)]+;*/, ""))
+            .join("; ");
+          setCookie(cookie);
+          clearInterval(timer.current);
+          closeCurrentWindow();
+          break;
+
+        default:
+          break;
+      }
+    }, 1000);
+  }
+
+  return (
+    <div
+      data-tauri-drag-region
+      className="w-full h-full pt-28 flex flex-col items-center"
+    >
+      <h1 className="text-3xl pb-4 font-bold">扫码登录</h1>
+      {[802, 803].includes(status) ? (
+        UserContent
+      ) : (
+        <QRContent qrimg={qrimg} status={status} onRefresh={login} />
+      )}
     </div>
   );
 });
